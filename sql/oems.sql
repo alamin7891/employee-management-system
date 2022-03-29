db3485be30a96784a8b411d37c21b8ec1c521ed0
alaminpbd <alamin.pbd20@gmail.com>
2022-03-29 12:45:27 +0600

    Updated database

--- sql/oems.sql
+++ sql/oems.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 15, 2022 at 08:24 AM
+-- Generation Time: Mar 29, 2022 at 08:43 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
@@ -71,12 +71,15 @@ INSERT INTO `department` (`id`, `department_code`, `department_name`, `created_b
 
 CREATE TABLE `empinfo` (
   `id` int(11) NOT NULL,
+  `employee_id` varchar(15) NOT NULL,
   `firstname` varchar(50) NOT NULL,
   `lastname` varchar(50) NOT NULL,
   `email` varchar(30) NOT NULL,
-  `password` varchar(20) NOT NULL,
   `gender` enum('Male','Female') NOT NULL,
+  `department_code` varchar(15) NOT NULL,
   `department` varchar(30) NOT NULL,
+  `designation` varchar(50) NOT NULL,
+  `salary` int(20) NOT NULL,
   `degree` varchar(30) NOT NULL,
   `phone` varchar(20) NOT NULL,
   `address` varchar(200) NOT NULL
@@ -86,18 +89,8 @@ CREATE TABLE `empinfo` (
 -- Dumping data for table `empinfo`
 --
 
-INSERT INTO `empinfo` (`id`, `firstname`, `lastname`, `email`, `password`, `gender`, `department`, `degree`, `phone`, `address`) VALUES
-(1, 'Waliul', 'Hasan', 'hasan@gmail.com', '', 'Male', 'CSE', 'BSC', '01524587972', 'North badda'),
-(13, 'Alamin', 'Hossain', 'alamin@gmail.com', '', 'Male', 'CSE', 'BSC', '01654646456', 'Modhubagh'),
-(14, 'Sayed', 'Rana', 'rana@gmail.com', '', 'Male', 'CSE', 'BSC', '6465464654', 'Kuril'),
-(15, 'Masum', 'Billal', 'masum@gmail.com', '', 'Male', 'CSE', 'BSC', '878775454', 'Kakrail'),
-(16, 'Md alamin', 'hossain', 'mdalamin@gmail.com', '', 'Male', 'CSE', 'BSC', '654645651', 'Jatrabari'),
-(17, 'Waliul', 'Hasan', 'hasan1@gmail.com', '', 'Male', 'CSE', 'BSC', '01524587972', 'North badda'),
-(18, 'Waliul', 'Hasan', 'hasan2@gmail.com', '', 'Male', 'CSE', 'BSC', '01524587972', 'North badda'),
-(19, 'Waliul', 'Hasan', 'hasan3@gmail.com', '', 'Male', 'CSE', 'BSC', '01524587972', 'North badda'),
-(20, 'Waliul', 'Hasan', 'hasan4@gmail.com', '', 'Male', 'CSE', 'BSC', '01524587972', 'North badda'),
-(23, 'Chapai', 'murshidabad', 'chapai@murshidabad.com', '', 'Female', 'Nai', 'Dhandabaj', '02114598', 'Dilhi'),
-(24, '', '', '', '', '', '', '', '', '');
+INSERT INTO `empinfo` (`id`, `employee_id`, `firstname`, `lastname`, `email`, `gender`, `department_code`, `department`, `designation`, `salary`, `degree`, `phone`, `address`) VALUES
+(1, 'emp002', 'Alamin', 'Hossain', 'alamin@gmail.com', 'Male', 'acc0001', 'Accounts', 'accounts', 25000, 'register', '01612168726', 'jatrabari, Dhaka');
 
 -- --------------------------------------------------------
 
@@ -126,6 +119,44 @@ CREATE TABLE `emp_leave` (
 INSERT INTO `emp_leave` (`id`, `employee_code`, `employee_name`, `reason`, `department_code`, `department`, `designation`, `start_date`, `end_date`, `total_days`, `comment`) VALUES
 (1, '', '', 'casual', '', 'Human Resource', 'accounts', '0000-00-00', '0000-00-00', '', 'fasfsafsa'),
 (2, '', '16', 'casual', '', 'Production', 'accounts', '0000-00-00', '0000-00-00', '', 'hdyhre');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `salary_slip`
+--
+
+CREATE TABLE `salary_slip` (
+  `id` int(11) NOT NULL,
+  `employee_id` varchar(50) NOT NULL,
+  `employee_name` varchar(50) NOT NULL,
+  `department_code` varchar(50) NOT NULL,
+  `department_name` varchar(50) NOT NULL,
+  `designation` varchar(50) NOT NULL,
+  `month` varchar(15) NOT NULL,
+  `basic_salary` int(11) NOT NULL,
+  `festival` varchar(50) NOT NULL,
+  `meal_allowance` varchar(15) NOT NULL,
+  `bonus` int(11) NOT NULL,
+  `loan` int(11) NOT NULL,
+  `house_rent` int(11) NOT NULL,
+  `total_allowances` int(11) NOT NULL,
+  `total_deduction` int(11) NOT NULL,
+  `net_salary` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `salary_slip`
+--
+
+INSERT INTO `salary_slip` (`id`, `employee_id`, `employee_name`, `department_code`, `department_name`, `designation`, `month`, `basic_salary`, `festival`, `meal_allowance`, `bonus`, `loan`, `house_rent`, `total_allowances`, `total_deduction`, `net_salary`) VALUES
+(1, '', '1', '', 'Finance', '', 'February', 0, '', '', 0, 0, 0, 0, 0, 0),
+(2, '', '1', '', 'Finance', '', 'February', 0, '', '', 0, 0, 0, 0, 0, 0),
+(3, '', 'emp002', '', 'Human Resource', 'accounts', 'February', 0, '750', '1800', 200, 2500, 500, 0, 0, 0),
+(4, '', 'emp002', '\r\nacc0001', 'Accounts', 'accounts', '', 25000, '750', '', 0, 0, 0, 0, 0, 0),
+(5, '', '', '', '', '', '', 0, '', '', 0, 0, 0, 0, 0, 0),
+(6, '', '', '', '', '', '', 0, '', '', 0, 0, 0, 0, 0, 0),
+(7, '', 'emp002', '\r\nacc0001', 'Accounts', 'accounts', '', 25000, '100', '122', 110, 250, 120, 332, 370, 24962);
 
 --
 -- Indexes for dumped tables
@@ -157,6 +188,12 @@ ALTER TABLE `emp_leave`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `salary_slip`
+--
+ALTER TABLE `salary_slip`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -176,13 +213,19 @@ ALTER TABLE `department`
 -- AUTO_INCREMENT for table `empinfo`
 --
 ALTER TABLE `empinfo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `emp_leave`
 --
 ALTER TABLE `emp_leave`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `salary_slip`
+--
+ALTER TABLE `salary_slip`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
